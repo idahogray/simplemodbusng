dd3d4c618255abfedc342265a63589cec73bad3c
Keith Gray <keith.gray@powereng.com>
2014-08-05 13:44:49 -0500

    Continued refactoring in order to add coil related function codes

--- SimpleModbusSlaveSoftwareSerial/SimpleModbusSlaveSoftwareSerial.cpp
+++ SimpleModbusSlaveSoftwareSerial/SimpleModbusSlaveSoftwareSerial.cpp
@@ -7,6 +7,12 @@ const char BROADCAST_ADDRESS = 0;
 const char SLAVE_ID_POSITION = 0;
 const char FUNCTION_CODE_POSITION = 1;
 const char BUFFER_ERROR = -1;
+const char READ_COILS = 1;
+const char READ_HOLDING_REGISTERS = 3;
+const char WRITE_SINGLE_COIL = 5;
+const char WRITE_SINGLE_REGISTER = 6;
+const char WRITE_MULTIPLE_COILS = 15;
+const char WRITE_MULTIPLE_REGISTERS = 16;
 
 // frame[] is used to recieve and transmit packages. 
 // The maximum serial ring buffer size is 128
@@ -105,14 +111,153 @@ bool is_broadcast_message()
   return slaveID == BROADCAST_ADDRESS;
 }
 
+bool verify_broadcast_and_function_code()
+{
+  if (broadcastFlag && (
+        function_code == READ_COILS || 
+        function_code == READ_HOLDING_REGISTERS))
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+bool destined_for_me()
+{
+  if (verify_slave_id_matches() || broadcastFlag)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void read_holding_registers(unsigned int *holdingRegs)
+{
+  // combine the starting address bytes
+  unsigned int startingAddress = ((frame[2] << 8) | frame[3]);
+  if (startingAddress < holdingRegsSize) // check exception 2 ILLEGAL DATA ADDRESS
+  {
+    exceptionResponse(2); // exception 2 ILLEGAL DATA ADDRESS
+  }
+
+  // combine the number of register bytes  
+  unsigned int no_of_registers = ((frame[4] << 8) | frame[5]);
+  unsigned int maxData = startingAddress + no_of_registers;
+  if (maxData <= holdingRegsSize) // check exception 3 ILLEGAL DATA VALUE
+  {
+    exceptionResponse(3); // exception 3 ILLEGAL DATA VALUE
+  }
+
+  unsigned char noOfBytes = no_of_registers * 2;
+  unsigned char responseFrameSize = 5 + noOfBytes; // ID, function, noOfBytes, (dataLo + dataHi) * number of registers, crcLo, crcHi
+  unsigned char address;
+  unsigned char index;
+  unsigned int crc16;
+  frame[0] = slaveID;
+  frame[1] = function_code;
+  frame[2] = noOfBytes;
+  address = 3; // PDU starts at the 4th byte
+  unsigned int temp;
+  
+  for (index = startingAddress; index < maxData; index++)
+  {
+    temp = holdingRegs[index];
+    frame[address] = temp >> 8; // split the register into 2 bytes
+    address++;
+    frame[address] = temp & 0xFF;
+    address++;
+  } 
+  
+  crc16 = calculateCRC(responseFrameSize - 2);
+  frame[responseFrameSize - 2] = crc16 >> 8; // split crc into 2 bytes
+  frame[responseFrameSize - 1] = crc16 & 0xFF;
+  sendPacket(responseFrameSize);
+}
+
+void write_single_register(unsigned int *holdingRegs)
+{
+  unsigned int startingAddress = ((frame[2] << 8) | frame[3]);
+  if (startingAddress < holdingRegsSize) // check exception 2 ILLEGAL DATA ADDRESS
+  {
+    exceptionResponse(2); // exception 2 ILLEGAL DATA ADDRESS
+  }
+  unsigned int regStatus = ((frame[4] << 8) | frame[5]);
+  unsigned char responseFrameSize = 8;
+  unsigned int crc16;
+  
+  holdingRegs[startingAddress] = regStatus;
+  
+  crc16 = calculateCRC(responseFrameSize - 2);
+  frame[responseFrameSize - 2] = crc16 >> 8; // split crc into 2 bytes
+  frame[responseFrameSize - 1] = crc16 & 0xFF;
+  sendPacket(responseFrameSize);
+}
+
+void write_multiple_registers(unsigned int *holdingRegs, unsigned char buffer_size)
+{
+  unsigned int startingAddress = ((frame[2] << 8) | frame[3]);
+  // combine the number of register bytes  
+  unsigned int no_of_registers = ((frame[4] << 8) | frame[5]);
+  unsigned int maxData = startingAddress + no_of_registers;
+  unsigned char address;
+  unsigned char index;
+  unsigned int crc16;
+  // check if the recieved number of bytes matches the calculated bytes minus the request bytes
+  // id + function_code + (2 * address bytes) + (2 * no of register bytes) + byte count + (2 * CRC bytes) = 9 bytes
+  if (frame[6] == (buffer_size - 9)) 
+  {
+    if (startingAddress < holdingRegsSize) // check exception 2 ILLEGAL DATA ADDRESS
+    {
+      if (maxData <= holdingRegsSize) // check exception 3 ILLEGAL DATA VALUE
+      {
+        address = 7; // start at the 8th byte in the frame
+        
+        for (index = startingAddress; index < maxData; index++)
+        {
+          holdingRegs[index] = ((frame[address] << 8) | frame[address + 1]);
+          address += 2;
+        } 
+        
+        // only the first 6 bytes are used for CRC calculation
+        crc16 = calculateCRC(6); 
+        frame[6] = crc16 >> 8; // split crc into 2 bytes
+        frame[7] = crc16 & 0xFF;
+        
+        // a function_code 16 response is an echo of the first 6 bytes from the request + 2 crc bytes
+        if (!broadcastFlag) // don't respond if it's a broadcast message
+          sendPacket(8); 
+      }
+      else  
+        exceptionResponse(3); // exception 3 ILLEGAL DATA VALUE
+    }
+    else
+      exceptionResponse(2); // exception 2 ILLEGAL DATA ADDRESS
+  }
+  else 
+    errorCount++; // corrupted packet
+}
+
 unsigned int modbus_update(unsigned int *holdingRegs, bool *coils)
 {
   unsigned char buffer_size = 0;
 
   buffer_size = process_serial_data();
-  if (buffer_size == -1)
+  if (buffer_size == BUFFER_ERROR)
+  {
     return errorCount++;
+  }
 	
+  if (!destined_for_me())
+  {
+    return errorCount;
+  }
+
   // The minimum request packet is 8 bytes for function 3 & 16
   if (verify_frame_size(buffer_size) == false)
   {
@@ -121,116 +266,31 @@ unsigned int modbus_update(unsigned int *holdingRegs, bool *coils)
 
   
   broadcastFlag = is_broadcast_message();
-  
-  // if the recieved ID matches the slaveID or broadcasting id (0), continue
-  if (verify_slave_id_matches() || broadcastFlag)
+  function_code = frame[FUNCTION_CODE_POSITION];
+  if (!verify_broadcast_and_function_code())
   {
-    if (verify_crc(buffer_size)) // if the calculated crc matches the recieved crc continue
-    {
-      function_code = frame[FUNCTION_CODE_POSITION];
-      unsigned int startingAddress = ((frame[2] << 8) | frame[3]); // combine the starting address bytes
-      unsigned int no_of_registers = ((frame[4] << 8) | frame[5]); // combine the number of register bytes  
-      unsigned int maxData = startingAddress + no_of_registers;
-      unsigned char index;
-      unsigned char address;
-      unsigned int crc16;
+      exceptionResponse(1); // exception 1 ILLEGAL FUNCTION
+      return errorCount++;
+  }
+
+  if (!verify_crc(buffer_size))
+  {
+    return errorCount++;
+  }
+
       
-      // broadcasting is not supported for function 3 
-      if (!broadcastFlag && (function_code == 3))
-      {
-        if (startingAddress < holdingRegsSize) // check exception 2 ILLEGAL DATA ADDRESS
-        {
-          if (maxData <= holdingRegsSize) // check exception 3 ILLEGAL DATA VALUE
-          {
-            unsigned char noOfBytes = no_of_registers * 2;
-            unsigned char responseFrameSize = 5 + noOfBytes; // ID, function, noOfBytes, (dataLo + dataHi) * number of registers, crcLo, crcHi
-            frame[0] = slaveID;
-            frame[1] = function_code;
-            frame[2] = noOfBytes;
-            address = 3; // PDU starts at the 4th byte
-            unsigned int temp;
-            
-            for (index = startingAddress; index < maxData; index++)
-            {
-              temp = holdingRegs[index];
-              frame[address] = temp >> 8; // split the register into 2 bytes
-              address++;
-              frame[address] = temp & 0xFF;
-              address++;
-            } 
-            
-            crc16 = calculateCRC(responseFrameSize - 2);
-            frame[responseFrameSize - 2] = crc16 >> 8; // split crc into 2 bytes
-            frame[responseFrameSize - 1] = crc16 & 0xFF;
-            sendPacket(responseFrameSize);
-          }
-          else  
-            exceptionResponse(3); // exception 3 ILLEGAL DATA VALUE
-        }
-        else
-          exceptionResponse(2); // exception 2 ILLEGAL DATA ADDRESS
-      }
-      else if (function_code == 6)
-      {
-        if (startingAddress < holdingRegsSize) // check exception 2 ILLEGAL DATA ADDRESS
-        {
-            unsigned int startingAddress = ((frame[2] << 8) | frame[3]);
-            unsigned int regStatus = ((frame[4] << 8) | frame[5]);
-            unsigned char responseFrameSize = 8;
-            
-            holdingRegs[startingAddress] = regStatus;
-            
-            crc16 = calculateCRC(responseFrameSize - 2);
-            frame[responseFrameSize - 2] = crc16 >> 8; // split crc into 2 bytes
-            frame[responseFrameSize - 1] = crc16 & 0xFF;
-            sendPacket(responseFrameSize);
-        }
-        else
-          exceptionResponse(2); // exception 2 ILLEGAL DATA ADDRESS
-        }
-      else if (function_code == 16)
-      {
-        // check if the recieved number of bytes matches the calculated bytes minus the request bytes
-        // id + function_code + (2 * address bytes) + (2 * no of register bytes) + byte count + (2 * CRC bytes) = 9 bytes
-        if (frame[6] == (buffer_size - 9)) 
-        {
-          if (startingAddress < holdingRegsSize) // check exception 2 ILLEGAL DATA ADDRESS
-          {
-            if (maxData <= holdingRegsSize) // check exception 3 ILLEGAL DATA VALUE
-            {
-              address = 7; // start at the 8th byte in the frame
-              
-              for (index = startingAddress; index < maxData; index++)
-              {
-                holdingRegs[index] = ((frame[address] << 8) | frame[address + 1]);
-                address += 2;
-              } 
-              
-              // only the first 6 bytes are used for CRC calculation
-              crc16 = calculateCRC(6); 
-              frame[6] = crc16 >> 8; // split crc into 2 bytes
-              frame[7] = crc16 & 0xFF;
-              
-              // a function_code 16 response is an echo of the first 6 bytes from the request + 2 crc bytes
-              if (!broadcastFlag) // don't respond if it's a broadcast message
-                sendPacket(8); 
-            }
-            else  
-              exceptionResponse(3); // exception 3 ILLEGAL DATA VALUE
-          }
-          else
-            exceptionResponse(2); // exception 2 ILLEGAL DATA ADDRESS
-        }
-        else 
-          errorCount++; // corrupted packet
-      }         
-      else
-        exceptionResponse(1); // exception 1 ILLEGAL FUNCTION
-    }
-    else // checksum failed
-      errorCount++;
-  } // incorrect id
-  return errorCount;
+  if (function_code == 3)
+  {
+    read_holding_registers(holdingRegs);
+  }
+  else if (function_code == 6)
+  {
+    write_single_register(holdingRegs);
+  }
+  else if (function_code == 16)
+  {
+    write_multiple_registers(holdingRegs, buffer_size);
+  }         
 }       
 
 void exceptionResponse(unsigned char exception)
